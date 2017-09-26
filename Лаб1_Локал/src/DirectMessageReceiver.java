a9a8e5747f83c75f9c0d8eb8f77ac247d96e6355
grisha9154 <grisha9154@gmail.com>
2017-09-26 23:17:32 +0300

    First task complite

--- Лаб1_Локал/src/DirectMessageReceiver.java
+++ Лаб1_Локал/src/DirectMessageReceiver.java
@@ -1,5 +1,8 @@
 import javax.jms.*;
 
+import com.sun.messaging.ConnectionConfiguration;
+import com.sun.messaging.ConnectionFactory;
+
 /**
  * Created by Grish on 26.09.2017.
  */
@@ -9,12 +12,34 @@ public class DirectMessageReceiver implements MessageListener {
 
    DirectMessageReceiver(){
        try(JMSContext context = factory.createContext("admin","admin")){
+        factory.setProperty(ConnectionConfiguration.imqAddressList,"mq://127.0.0.1:7676,mq://127.0.0.1:7676");
+        Destination cardsQueue = context.createQueue("BankCardQueue");
+        consumer = context.createConsumer(cardsQueue);
+        consumer.setMessageListener(this);
+           System.out.println("Listening to theBankCardQueue...");
 
+           Thread.sleep(100000);
+       } catch (JMSException e) {
+           System.out.println("Error: " + e.getMessage());
+       } catch (InterruptedException e) {
+           System.out.println("Error: " + e.getMessage());
        }
    }
 
     @Override
     public void onMessage(Message message) {
+        try{
+            System.out.println("Got the text message from the BankCardQueue: "+ message.getBody(String.class));
 
+            System.out.println("\n =Here's what toString() on the message prints \n" + message);
+        } catch (JMSException e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
     }
+public static void main(String[] args){
+       new DirectMessageReceiver();
 }
+}
+
+
